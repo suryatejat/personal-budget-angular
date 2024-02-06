a62babf957fe4c58c15539688cdffd08d995bd91
Surya <suryat2020@gmail.com>
2024-02-06 11:50:29 -0500

    Added cors to budget api.

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+// Budget API
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
-
 const budget = require('./budgetData.json');
 
-app.use("/", express.static('public'))
+app.use(cors());
 
 app.get("/budget", (req, res) => {
     res.json(budget);
 })
 
 app.listen(port, () => {
-    console.log(`Listining to http://localhost:${port}`)
+    console.log(`API served at http://localhost:${port}`)
 })
